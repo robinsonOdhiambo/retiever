2e0e582611a035dfe33b8e8ed49fc2b17c79ab6e
dev_robo <dev.robo.odhis@gmail.com>
2017-10-26 05:53:15 +0300

    First initial commit

---
+++ src/main/java/com/thehowtheory/retriever/App.java
@@ -0,0 +1,13 @@
+package com.thehowtheory.retriever;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
